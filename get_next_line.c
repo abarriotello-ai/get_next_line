5dab7f07697d0b3c92ec7113cf9cbbed4ebbb6b4
Alonso Barrio <abarriotello@gmail.com>
2025-12-01 20:53:43 +0100

    ft_extrac_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,39 @@
 /*   By: abarrio <abarrio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 12:02:04 by abarrio           #+#    #+#             */
-/*   Updated: 2025/11/28 12:16:26 by abarrio          ###   ########.fr       */
+/*   Updated: 2025/12/01 20:18:46 by abarrio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_extract_line(char *stash)
+{
+	char	*line;
+	size_t	i;
+
+	if (stash == NULL || stash[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\0' && stash[i] != '\n')
+		i++;
+	if (stash[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	while (*stash != '\0' && *stash != '\n')
+		*(line++) = *(stash++);
+	if (*stash == '\n')
+		*(line++) = *(stash++);
+	*line = '\0';
+	return (line);
+}
+
+static char	*ft_clean_stash(char *stash)
+{
+	
+}
 
 char	*get_next_line(int fd)
 {
@@ -18,5 +48,5 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	stash = NULL;
-	
+
 }
